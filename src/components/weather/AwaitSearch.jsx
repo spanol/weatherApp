5d8324d17b38a09c885e0b202541cc37fb682b09
spanol <viniciusspanol@gmail.com>
2022-04-12 14:01:35 -0300

    commit
    commit

--- src/components/weather/AwaitSearch.jsx
+++ src/components/weather/AwaitSearch.jsx
@@ -2,8 +2,6 @@ const AwaitSearch = () => {
   return (
     <div>
       <h1>To start, enter a city...</h1>
-      <button onClick={()=>{console.log(process.env.APIKEY)}}>teste</button>
-
     </div>
   );
 };
